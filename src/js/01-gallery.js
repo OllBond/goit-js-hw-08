c73e517cd65682e6473d12d7bf7d9e4b326e2c53
Olena <newsroommy@gmail.com>
2022-11-15 14:23:41 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,57 @@
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector(".gallery");
+
+const newPicturesMarkup = createNewGallaryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", newPicturesMarkup);
+
+function pictures ({ preview, original, description }) {
+  return `
+<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+<img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+/>
+</a>
+</li>
+`;
+}
+
+function createNewGallaryItems(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+    })
+    .join("");
+}
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    'captionsData': 'alt',
+    'captionDelay': 2500, 
+  });
+
