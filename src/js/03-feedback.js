2824160f7e5aa9a8e1a603af76f544f782eba020
Olena <newsroommy@gmail.com>
2022-11-18 11:36:54 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
 
+import throttle from "lodash.throttle";
+const ref = {
+    form: document.querySelector('.feedback-form'),
+    // input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+}
+
+// const formData = {};
+ref.form.addEventListener('submit', onFormSubmit);
+ref.textarea.addEventListener('input', onTextareaInput);
+
+function onFormSubmit (e) {
+e.preventDefault();
+
+e.currentTarget.reset();
+
+}
+
+function onTextareaInput (e) {
+const message = e.target.value;
+localStorage.setItem('feedback-form-state', message);
+}
+
